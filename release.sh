fcfe3a55937cb4bb7bfcf4ee6a49a360bd9d366c
root <root@localhost>
2018-10-15 14:59:13 +0000

    Release version 0.0.2

--- release.sh
+++ release.sh
@@ -1,8 +1,4 @@
-#!/usr/bin/env bash
-# build and release a new deployment of marionette to the marionette ECS registry in nexmo-dev
-IMAGE=marionette
-
-while getopts ":r:d" opt; do
+while getopts ":r:" opt; do
   case $opt in
     r)
       # echo "-ra was triggered, Parameter: $OPTARG" >&2
@@ -12,9 +8,6 @@ while getopts ":r:d" opt; do
       echo "Invalid option: -$OPTARG" >&2
       exit 1
       ;;
-    d)
-      DEPLOY=1
-      ;;   
     :)
       echo "Option -$OPTARG requires an argument." >&2
       exit 1
@@ -27,17 +20,19 @@ if [[ -z "${REPO}" ]]; then
     exit 1
 fi
 
+IMAGE=marionette
 
 version=`cat .version`
-
 if [[ -z "${version}" ]]; then
     echo "Could not determine version, aborting."
-    exit 1
 fi
 
 echo "version: $version"
-docker build . -t $version\
-docker tag $IMAGE:$version $IMAGE:latest 
+docker build . -t $IMAGE:$version
+docker tag $IMAGE:$version $IMAGE:latest
+
+# tag it
+pushd .
 
 git add -A
 git commit -m "Release version $version"
@@ -46,15 +41,7 @@ git push
 git push --tags
 
 docker tag $IMAGE:latest $REPO/$IMAGE:latest
-docker tag $IMAGE:$version $REPO/$IMAGE:$version 
+docker tag $IMAGE:$version $REPO/$IMAGE:$version
 docker push $REPO/$IMAGE:latest
 docker push $REPO/$IMAGE:$version
 
-echo "Release ${REPO}/${IMAGE} version $version built and pushed"
-
-if [[ -n "${DEPLOY}" ]]; then
-  echo "Initiating deployment to kubernetes cluster"
-  kubectl set image deployment/$image image=image:$version --record
-
-  kubectl rollout status deployment/nginx-deployment
-fi
