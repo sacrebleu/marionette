36bffda57f678559ac339c944a34b97a819c5ae1
Jeremy Botha <jeremy.botha@vonage.com>
2018-10-15 15:56:23 +0100

    Move structure around

--- release.sh
+++ release.sh
@@ -1,4 +1,8 @@
-while getopts ":r:" opt; do
+#!/usr/bin/env bash
+# build and release a new deployment of marionette to the marionette ECS registry in nexmo-dev
+IMAGE=marionette
+
+while getopts ":r:d" opt; do
   case $opt in
     r)
       # echo "-ra was triggered, Parameter: $OPTARG" >&2
@@ -8,6 +12,9 @@ while getopts ":r:" opt; do
       echo "Invalid option: -$OPTARG" >&2
       exit 1
       ;;
+    d)
+      DEPLOY=1
+      ;;   
     :)
       echo "Option -$OPTARG requires an argument." >&2
       exit 1
@@ -20,19 +27,17 @@ if [[ -z "${REPO}" ]]; then
     exit 1
 fi
 
-IMAGE=marionette
 
 version=`cat .version`
+
 if [[ -z "${version}" ]]; then
     echo "Could not determine version, aborting."
+    exit 1
 fi
 
 echo "version: $version"
-docker build . -t $IMAGE:$version
+docker build . -t $version\
 docker tag $IMAGE:$version $IMAGE:latest 
-
-# tag it
-pushd .
 
 git add -A
 git commit -m "Release version $version"
@@ -45,3 +50,11 @@ docker tag $IMAGE:$version $REPO/$IMAGE:$version
 docker push $REPO/$IMAGE:latest
 docker push $REPO/$IMAGE:$version
 
+echo "Release ${REPO}/${IMAGE} version $version built and pushed"
+
+if [[ -n "${DEPLOY}" ]]; then
+  echo "Initiating deployment to kubernetes cluster"
+  kubectl set image deployment/$image image=image:$version --record
+
+  kubectl rollout status deployment/nginx-deployment
+fi
