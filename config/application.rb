f17a7b6a26fcb89ba1ebd5bd388d3030e5a6422b
Jeremy Botha <jeremy.botha@vonage.com>
2018-10-15 14:32:08 +0100

    Added version to ui

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module Marionette
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.marionette_version = File.read("#{Rails.root}/.version").strip
   end
 end
