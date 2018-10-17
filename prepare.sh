7fd041208dce6ac1fe63b092bf486fadb3e7b818
Jeremy Botha <jeremy.botha@vonage.com>
2018-10-17 13:37:05 +0100

    Releasing 0.0.13

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-sudo apt-get update 
+echo "Installing awscli and kubectl"
+
 pip install --local awscli
-sudo apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get install -y kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+curl -LO https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+sudo mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
+
 export PATH=$PATH:$HOME/.local/bin
+
+echo "Done"
