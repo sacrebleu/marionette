7b43f0e99682e645fe5d516b34deeee68bdb3c4e
Jeremy Botha <jeremy.botha@vonage.com>
2018-10-17 13:46:59 +0100

    Releasing 0.0.14

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-echo "Installing awscli and kubectl"
+echo "Installing awscli"
 
-pip install --local awscli
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+
+echo "Installing kubectl"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
+echo "Installing aws-iam-authenticator"
 curl -LO https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-07-26/bin/linux/amd64/aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
 sudo mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
